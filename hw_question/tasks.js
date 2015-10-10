066935c832311f7f5ca6c6e1ff3ca3d3a4712ba6
Mike Chekov <waden@bk.ru>
2015-10-10 13:08:55 +0300

    Add to do howeword not continue for quetion

---
+++ hw_question/tasks.js
@@ -0,0 +1,45 @@
+var render = require('../render.js');
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host     : '127.0.0.1',
+      user     : 'root',
+      password : '',
+      database : 'testnodebd',
+      charset  : 'utf8'
+    },
+    pool: {
+      min: 0,
+      max: 7
+    }
+});
+
+var Tasks = {
+	list: function(callback) {
+		knex.select('task').from('tasks').asCallback(function(err, tasks) {
+      		if (err) return console.error(err);
+
+      		tasks
+
+      	});
+	},
+
+	add: function(task, callback) {
+		knex('tasks').insert({task: task});
+	},
+	
+	change: function(id, text, callback) {
+		// TODO
+	},
+
+	complete: function(id, callback) {
+		// TODO
+	},
+
+	delete: function(id, callback) {
+		// TODO
+	}
+};
+
+module.exports = Tasks;
---
+++ hw_question/todo.js
@@ -0,0 +1,48 @@
+var tasks = require('./models/tasks');
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
+var templates = require('consolidate');
+app.engine('hbs',  templates.handlebars);
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+//app.use(express.static(__dirname));
+
+//var request = require('request');
+var urlutils = require('url');
+var cheerio = require('cheerio');
+var $ = cheerio;
+
+app.get('/', function(req, res) {
+	tasks.list(function(err, tasks) {
+		if (err) console.log(err);
+
+		console.dir(tasks);
+
+		templates.handlebars(
+			'views/tasks.hbs', 
+			{tasks: tasks},
+			function(err, html) {
+				if (err) 
+					throw err;
+
+				console.log(html);
+
+				res.render('layout.hbs', {
+					content: html
+				});
+			}
+		)
+	});
+});
+
+app.post('/', function(req, res) {
+	tasks.add(req.body.task, function() {
+		res.redirect('/');
+	});
+});
+app.listen(3000);
