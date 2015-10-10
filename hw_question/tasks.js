21cf5ebe1fb0ce2962aa4248e883f8ad2c3c7865
Mike Chekov <waden@bk.ru>
2015-10-10 13:36:16 +0300

    Added changes

--- hw_question/tasks.js
+++ hw_question/tasks.js
@@ -1,11 +1,10 @@
-var render = require('../render.js');
 
 var knex = require('knex')({
     client: 'mysql',
     connection: {
       host     : '127.0.0.1',
       user     : 'root',
-      password : '',
+      password : 'wpcrud47',
       database : 'testnodebd',
       charset  : 'utf8'
     },
@@ -20,7 +19,7 @@ var Tasks = {
 		knex.select('task').from('tasks').asCallback(function(err, tasks) {
       		if (err) return console.error(err);
 
-      		tasks
+      		callback(tasks);
 
       	});
 	},
